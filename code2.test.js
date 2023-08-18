b4cf370f60ff83f3865afb8362eb1a1769ef5a33
Misha Grinko <glukym@gmail.com>
2023-08-18 09:39:17 +0300

    Update code2.test.js

--- code2.test.js
+++ code2.test.js
@@ -1,25 +1,35 @@
-
-describe('takeEveryThirdLetter', () => {
-  const takeEveryThirdLetter = require('./code2');
+describe('takeEveryThirdCharacter', () => {
+  const takeEveryThirdCharacter = require('./code2');
 
   it('is a function', () => {
-    expect(typeof takeEveryThirdLetter).toBe('function');
+    expect(typeof takeEveryThirdCharacter).toBe('function');
+  });
+
+  it('returns every 3rd letter', () => {
+    expect(takeEveryThirdCharacter('qwertyuiop')).toBe('eyo');
+  });
+
+  it('returns every 3rd digit', () => {
+    expect(takeEveryThirdCharacter('1234567890')).toBe('369');
   });
 
   it('returns empty string for empty input', () => {
-    expect(takeEveryThirdLetter('')).toBe('');
+    expect(takeEveryThirdCharacter('')).toBe('');
   });
 
   it('returns empty string for input with less than 3 characters', () => {
-    expect(takeEveryThirdLetter('ab')).toBe('');
+    expect(takeEveryThirdCharacter('ab')).toBe('');
   });
 
-  it('returns every third letter for input with length multiple of 3', () => {
-    expect(takeEveryThirdLetter('abcdef')).toBe('cf');
+  it('returns the last character for 3 letters word', () => {
+    expect(takeEveryThirdCharacter('and')).toBe('d');
   });
 
-  it('returns every third letter for input with length not multiple of 3', () => {
-    expect(takeEveryThirdLetter('abcdefgh')).toBe('cf');
+  it('returns every third character for input with length multiple of 3', () => {
+    expect(takeEveryThirdCharacter('abcdef')).toBe('cf');
   });
 
+  it('returns every third character for input with length not multiple of 3', () => {
+    expect(takeEveryThirdCharacter('abcdefgh')).toBe('cf');
+  });
 });
